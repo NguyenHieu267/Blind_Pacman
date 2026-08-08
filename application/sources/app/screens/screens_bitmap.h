008c2fa73c608701563c8e4f90251b103516dac9
NguyenHieu267 <hieunguyen11l121@gmail.com>
2026-08-09 00:24:35 +0700

    [UPDATE] Bitmap for characters  + setting menu

--- application/sources/app/screens/screens_bitmap.h
+++ application/sources/app/screens/screens_bitmap.h
@@ -3,7 +3,17 @@
 
 #include "view_render.h"
 
-// scr_welcome
-extern const unsigned char PROGMEM bitmap_dolphin[];
+const unsigned char PROGMEM bitmap_pacman_closed[];
+const unsigned char PROGMEM bitmap_pacman_open[];
+const unsigned char PROGMEM bitmap_ghost_2legs[];
+const unsigned char PROGMEM bitmap_ghost_3legs[];
+const unsigned char PROGMEM bitmap_wall_8x8[];
+const unsigned char PROGMEM bitmap_cherry[];
+const unsigned char PROGMEM bitmap_ghost_scared[];
+
+const unsigned char PROGMEM icon_play[];
+const unsigned char PROGMEM setting_icon[];
+const unsigned char PROGMEM ranking_icon[];
+const unsigned char PROGMEM exit_icon[];
 
 #endif //__SCREENS_BITMAP_H__

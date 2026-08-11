9ce21d1d8fee215da67f6edd7c453ebad82065d7
NguyenHieu267 <hieunguyen11l121@gmail.com>
2026-08-11 18:51:02 +0700

    [UPDATE] Welcome screen

--- application/sources/app/screens/scr_welcome.cpp
+++ application/sources/app/screens/scr_welcome.cpp
@@ -1,22 +1,9 @@
 #include "scr_welcome.h"
 
-#define WELCOME_TEXT_LINE_1_LEN		(8)
-#define WELCOME_TEXT_LINE_2_LEN		(5)
-#define WELCOME_TEXT_TOTAL_LEN		(WELCOME_TEXT_LINE_1_LEN + WELCOME_TEXT_LINE_2_LEN)
-
-static uint8_t welcome_text_index = 0;
-static const char* welcome_text_line_1 = "Welcome";
-static const char* welcome_text_line_2 = "to AK";
+static uint8_t welcome_pac_x = 0; 
+static uint16_t anim_tick = 0;
 
 static void view_scr_welcome();
-
-static void welcome_print_text_partial(const char* text, uint8_t max_chars) {
-	if (text == (const char*)0) return;
-
-	for (uint8_t i = 0; i < max_chars && text[i] != '\0'; i++) {
-		view_render.print(text[i]);
-	}
-}
 
 view_dynamic_t dyn_view_welcome = {
 	{
@@ -33,65 +20,62 @@ view_screen_t scr_welcome = {
 	.focus_item = 0,
 };
 
-void view_scr_welcome() {
+void view_scr_welcome(){
 	view_render.clear();
-	view_render.drawBitmap(	0, \
-		0, \
-		bitmap_pacman_open, \
-		8, \
-		8, \
-		WHITE);
+	// PACMAN
+	view_render.setTextSize(2);
+    view_render.setTextColor(WHITE);
+    view_render.setCursor(28, 15);
+    view_render.print("PACMAN");
 
+	// Ghost chase Pacman
+	int p1_x = welcome_pac_x;			// Pacman 
+    int g1_x = welcome_pac_x - 16;		// Ghost is 16 pixels far from Pacman 
+	for (int dx = 0; dx < 128; dx += 12){      // Draw bean
+    	if(dx > p1_x + 6){
+            view_render.fillRect(dx, 35, 2, 2, WHITE);
+        }
+    }
+	view_render.drawBitmap(
+        p1_x, 32, 
+        (anim_tick%2)?bitmap_pacman_open:bitmap_pacman_closed,      // Draw bitmap pacman open + closed
+        8, 
+        8, 
+        WHITE
+    );
+    view_render.drawBitmap(
+        g1_x, 32, 
+        (anim_tick%2)?bitmap_ghost_2legs:bitmap_ghost_3legs,        // Draw ghost running
+        8, 
+        8, 
+        WHITE
+    );
+	
+	// Press MODE ->
 	view_render.setTextSize(1);
-	view_render.setTextColor(WHITE);
-	view_render.setCursor(76, 12);
-	welcome_print_text_partial(welcome_text_line_1, welcome_text_index);
-	view_render.setCursor(84, 25);
-	if (welcome_text_index > WELCOME_TEXT_LINE_1_LEN) {
-		welcome_print_text_partial(welcome_text_line_2, welcome_text_index - WELCOME_TEXT_LINE_1_LEN);
-	}
+    view_render.setCursor(18, 45);
+    view_render.print("Press MODE ->");
 }
 
-void scr_welcome_handle(ak_msg_t *msg) {
-	switch (msg->sig) {
-	case SCREEN_ENTRY: {
+void scr_welcome_handle(ak_msg_t *msg){
+	switch (msg->sig){
+	case SCREEN_ENTRY:{
 		APP_DBG_SIG("SCREEN_ENTRY\n");
-		welcome_text_index = 0;
-		BUZZER_PlaySound(BUZZER_SOUND_WELCOME);
-		timer_set(AC_TASK_DISPLAY_ID, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK_INTERVAL, TIMER_PERIODIC);
-		timer_set(AC_TASK_DISPLAY_ID, AC_DISPLAY_SHOW_IDLE, AC_DISPLAY_IDLE_INTERVAL, TIMER_ONE_SHOT);
+		welcome_pac_x = 0;
+        timer_set(AC_TASK_DISPLAY_ID, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK, 120, TIMER_PERIODIC);
 	} break;
 
-	case AC_DISPLAY_WELCOME_TEXT_ANIM_TICK: {
-		APP_DBG_SIG("AC_DISPLAY_WELCOME_TEXT_ANIM_TICK\n");
-		if (welcome_text_index < WELCOME_TEXT_TOTAL_LEN) {
-			welcome_text_index++;
-		}
-		else {
-			timer_remove_attr(AC_TASK_DISPLAY_ID, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK);
-		}
-	} break;
+	case AC_DISPLAY_WELCOME_TEXT_ANIM_TICK:{
+        anim_tick++;
+        welcome_pac_x += 4;
+        if (welcome_pac_x >= 144) welcome_pac_x -= 144; 
 
-	case AC_DISPLAY_BUTON_MODE_PRESSED: {
-		APP_DBG_SIG("AC_DISPLAY_BUTON_MODE_PRESSED\n");
-		timer_remove_attr(AC_TASK_DISPLAY_ID, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK);
-		SCREEN_TRAN(scr_idle_handle, &scr_idle);
-	} break;
+    } break;
 
-	case AC_DISPLAY_SHOW_IDLE: {
-		APP_DBG_SIG("AC_DISPLAY_SHOW_IDLE\n");
-		timer_remove_attr(AC_TASK_DISPLAY_ID, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK);
-		SCREEN_TRAN(scr_idle_handle, &scr_idle);
-	} break;
+    case AC_DISPLAY_BUTON_MODE_PRESSED:{ 
+        timer_remove_attr(AC_TASK_DISPLAY_ID, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK);
+    } break;
 
-	case AC_DISPLAY_BUTON_UP_PRESSED:
-	case AC_DISPLAY_BUTON_DOWN_PRESSED: {
-		APP_DBG_SIG("AC_DISPLAY_BUTON_%s_PRESSED\n", msg->sig == AC_DISPLAY_BUTON_UP_PRESSED ? "UP" : "DOWN");
-		timer_remove_attr(AC_TASK_DISPLAY_ID, AC_DISPLAY_WELCOME_TEXT_ANIM_TICK);
-		SCREEN_TRAN(scr_qrcode_handle, &scr_qrcode);
-	} break;
-
-	default:
-		break;
-	}
+    default: break;
+    }
 }

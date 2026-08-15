9dd8c50dc0a055f6f8011aa227993c3d74f76511
NguyenHieu267 <hieunguyen11l121@gmail.com>
2026-08-15 08:49:52 +0700

    [UPDATE] Refactor pm_game_ghost

--- application/sources/app/pacman/pm_game.h
+++ application/sources/app/pacman/pm_game.h
@@ -44,4 +44,9 @@ extern int time_played;
 extern uint16_t anim_tick;
 
 extern void pm_game_core_handle(ak_msg_t* msg);
+
+// pm_game_ghost
+extern void pm_ghost_init();
+extern void pm_ghosts_update_move(bool is_frightened);
+extern void pm_ghost_check_collision(bool is_frightened);
 #endif // __PM_GAME_H__
--- application/sources/app/pacman/pm_game_ghost.h
+++ application/sources/app/pacman/pm_game_ghost.h
@@ -1,11 +1,6 @@
-// --- pm_game_ghost.h ---
 #ifndef __PM_GAME_GHOST_H__
 #define __PM_GAME_GHOST_H__
 
 #include "pm_game.h"
 
-extern void pm_ghost_init();
-extern void pm_ghosts_update_move(bool is_frightened);
-extern void pm_ghost_check_collision(bool is_frightened);
-
-#endif
+#endif //__PM_GAME_GHOST_H__
